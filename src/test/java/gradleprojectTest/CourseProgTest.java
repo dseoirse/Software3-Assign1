c9bf06ef91f13ef265285d35371dfa420834650c
dseoken <d.seo.kenn@gmail.com>
2021-11-01 16:21:49 +0000

    rename test package

--- src/test/java/gradleprojectTest/CourseProgTest.java
+++ src/test/java/gradleprojectTest/CourseProgTest.java
@@ -3,10 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gradleproject1;
+package gradleprojectTest;
 
 import gradleproject1.CourseProg;
+import gradleproject1.CourseProg;
 import gradleproject1.Module;
+import gradleproject1.Module;
+import gradleproject1.Student;
 import gradleproject1.Student;
 import java.util.ArrayList;
 import org.joda.time.DateTime;
--- src/test/java/gradleprojectTest/StudentTest.java
+++ src/test/java/gradleprojectTest/StudentTest.java
@@ -3,11 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gradleproject1;
+package gradleprojectTest;
 
 import gradleproject1.Student;
 import gradleproject1.CourseProg;
+import gradleproject1.CourseProg;
 import gradleproject1.Module;
+import gradleproject1.Module;
+import gradleproject1.Student;
 import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
